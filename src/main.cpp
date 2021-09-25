4c09ba516b8334b1bf8241ef38685cf2663c29d4
wavecorner <48245861+wavecorner@user.noreply.github.com>
2021-09-25 16:41:35 +0200

    Add working proof of concept: sleep + RTC
    Added ccls in personal setup and thus also to gitignore.
    
    The `main.cpp` now contains a working script that will run pumps for a
    given amount of time every *N* times it wakes up from deep sleep.
    
    Next step is some hardware calibration.

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,127 @@
 #include <Arduino.h>
 #include <Adafruit_MCP3008.h>
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include <SD.h>
+//#include <PubSubClient.h>
 #include <SPI.h>
 
-// reconfigure the built-in ADC
-ADC_MODE(ADC_VCC);
+#define debug
 
-// function declarations
-void callback(String topic, byte* message, uint length);
-void reconnect();
+Adafruit_MCP3008 adc;
 
-// MQTT
-const char* mqtt_server("172.18.100.106");
-WiFiClient espClient;
-PubSubClient client(mqtt_server, 1883, callback, espClient);
+// Chip Select pin of shift register
+static const int CS_shiftreg{ 4 };
+// Output Enable of shift register
+static const int OEpump{ 5 };
+// number of attached pumps
+static const int num_channels{ 4 };
 
-//Adafruit_MCP3008 adc;
 
-// global variables
-static const int SD_CS=4;
+// duration of water flowing
+static const int pump_delay{ 3 };
+// how many wake intervals are skipped before checking plants again
+static const int wake_skip{ 7 }; //TODO set this!
+// duration of deep sleep
+static const unsigned long long sleep_duration{ 6000000 }; // 6 Seconds
+//static const unsigned long long sleep_duration{ ESP.deepSleepMax() };
+
+// critical water level per plant in percent
+static int water_levels[4] { 20, 40, 60, 80 }; //TODO calibrate this somehow
 
 void setup() {
+    // read current wake interval
+    uint wake_interval;
+    bool read_rtc = ESP.rtcUserMemoryRead(0, &wake_interval, sizeof(wake_interval));
+
+#ifdef debug
+    // start serial debug
+    Serial.begin(115200);
+    while(!Serial);
+    Serial.println("Counter is: " + String(wake_interval));
+#endif
+
+    // make sure that counter doesn't go through the roof
+    if (wake_interval > wake_skip)
+        wake_interval = 0;
+
+    // we only want to do something every N times the MCU wakes up
+    if (wake_interval % wake_skip != 0) {
+#ifdef debug
+        Serial.println("Going to sleep - counter is only: " + String(wake_interval));
+#endif
+        // increment counter
+        ++wake_interval;
+        // write to RTC memory
+        ESP.rtcUserMemoryWrite(0, &wake_interval, sizeof(wake_interval));
+        ESP.deepSleep(sleep_duration, WAKE_RF_DISABLED);
+    }
+    else
+        wake_interval = 0;
+
     // keep WiFi off while we're not transmitting
     WiFi.mode(WIFI_OFF);
     WiFi.forceSleepBegin();
     delay(1);
 
-    Serial.begin(115200);
-    while(!Serial);
+    // disable output for the pump driver
+    pinMode(OEpump, OUTPUT);
+    digitalWrite(OEpump, HIGH);
 
-    pinMode(2, OUTPUT);
-    digitalWrite(2, HIGH);
+    // CS for shift register
+    pinMode(CS_shiftreg, OUTPUT);
+    digitalWrite(CS_shiftreg, HIGH);
 
-    int VCC{ ESP.getVcc() };
-    Serial.println("Vcc = " + String(VCC));
 
-    // turn WiFi on and transmit data 
+    // read ADC channels 0-3
+    adc.begin(15); //TODO this should not be hard coded
+
+    int readings[num_channels]{};
+    for (int i=0; i<4; i++) {
+        readings[i] = adc.readADC(i) * 100 / 1023;
+#ifdef debug
+        Serial.println("Channel " + String(i) + ": " + String(readings[i]));
+#endif
+    }
+
+    // turn on pumps as needed
+    byte pumps{ 0 };
+    // determine status for each pump
+    for (int i=0; i<num_channels; i++) {
+        if (readings[i] <= water_levels[i])
+            bitWrite(pumps, i, 1);
+        else
+            bitWrite(pumps, i, 0);
+    }
+
+    /*** write to shift register ***/
+    // activate shift register
+    digitalWrite(CS_shiftreg, LOW);
+    // transaction
+    SPI.beginTransaction(SPISettings(14000000, LSBFIRST, SPI_MODE0));
+    SPI.transfer(pumps);
+    SPI.endTransaction();
+    // deactivate shift register
+    digitalWrite(CS_shiftreg, HIGH);
+
+    // turn pumps on for certain time
+    digitalWrite(OEpump, LOW);
+#ifdef debug
+    Serial.println("Pumps on!");
+#endif
+    delay(pump_delay * 1000);
+    digitalWrite(OEpump, HIGH);
+#ifdef debug
+    Serial.println("Pumps off!");
+#endif
+
+#ifdef MQTT
+    // turn WiFi on and transmit data
     WiFi.forceSleepWake();
     delay(1);
-    WiFi.persistent(false);
+    WiFi.persistent(false); // prevent credentials from being written to EEPROM
     WiFi.mode(WIFI_STA);
     WiFi.begin(SSID, PASS);
 
+#ifdef debug
     Serial.print("ESP ");
     Serial.println(WiFi.macAddress());
     Serial.print("Connecting to ");
@@ -56,91 +134,25 @@ void setup() {
     Serial.println();
     Serial.print("WiFi connected - ESP IP address: ");
     Serial.println(WiFi.localIP());
+#endif
 
-    // MQTT connection
-    if (!client.connected())
-        reconnect();
-
-    // publish value to MQTT server
-    if (client.publish("room/plants", String(VCC).c_str()))
-        Serial.println("published");
-    for (int i=0; i<10; i++) {
-        client.loop();
-        delay(100);
-    }
-
-    client.disconnect();
-    if (!client.connected())
-        Serial.println("disconnected");
-
-    delay(1000);
     // disconnect from wifi and go to deep sleep
     WiFi.disconnect(true);
+#endif
+
     delay(1);
-    Serial.println("going to sleep");
-    ESP.deepSleep(6e6, WAKE_RF_DISABLED);
-    //adc.begin();
-    //Serial.println("MCP3008 setup performed.");
 
-    //Serial.println("Setup complete");
+#ifdef debug
+    Serial.println("going to sleep for: " + String(sleep_duration));
+#endif
+    // TODO this is duplicate code
+    // increment counter
+    ++wake_interval;
+    // write to RTC memory
+    ESP.rtcUserMemoryWrite(0, &wake_interval, sizeof(wake_interval));
+    // sleep as long as possible
+    ESP.deepSleep(sleep_duration, WAKE_RF_DISABLED);
 }
 
-void loop() {
-    /*
-    Serial.print("Channel " + String(chan) + ": ");
-    int val{ adc.readADC(chan) };
-
-    Serial.println(val);
-    delay(1000);
-    */
-}
-
-void callback(String topic, byte* message, uint length) {
-    Serial.print("Message arrived on topic: ");
-    Serial.print(topic);
-    Serial.print(". Message: ");
-    String messageTemp;
-
-    for (uint i=0; i<length; i++) {
-        Serial.print((char)message[i]);
-        messageTemp += (char)message[i];
-    }
-    Serial.println();
-
-    if (topic=="room/lamp") {
-        Serial.print("Changing room lamp to ");
-        if (messageTemp == "on") {
-            digitalWrite(2, LOW);
-            Serial.println("ON");
-        }
-        else if (messageTemp == "off") {
-            digitalWrite(2, HIGH);
-            Serial.println("OFF");
-        }
-    }
-}
-
-void reconnect() {
-    while (!client.connected()) {
-        Serial.println("Attempting MQTT connection...");
-
-        if (client.connect("ESP8266APWS")) {
-            Serial.println("connected");
-            client.subscribe("room/lamp");
-        }
-        else {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" trying again in 5 seconds");
-            delay(5000);
-        }
-    }
-}
-
-/*
-Idea: 
-- read moisture every hour 
-- once a week water the plant, unless critical moisture value is exceeded
-- if critical dryness is exceeded, water immediately
-- keep track of used volume of water (stop watering when no water left)
-*/
+// loop will stay empty - we only run single shots between deep sleeps
+void loop() {}
